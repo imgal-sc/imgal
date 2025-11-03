f7abd3ed07f3f976c10a69802a5d469a8b2abc92
Edward Evans <elevans2@wisc.edu>
2025-11-03 11:30:52 -0600

    Add inverse_normal_cdf function test

--- imgal/tests/test_distribution.rs
+++ imgal/tests/test_distribution.rs
@@ -10,3 +10,15 @@ fn distribution_gaussian() {
     assert_eq!(gauss_arr[100], 0.004465507286912305);
     assert_eq!(midpoint(&gauss_arr, None), 1.0000000000000007);
 }
+
+#[test]
+fn distribution_inverse_normal_cdf() {
+    // store undefined result for assert later
+    let undefined_result = distribution::inverse_normal_cdf(0.0).unwrap();
+
+    // assert function results
+    assert_eq!(distribution::inverse_normal_cdf(0.975).unwrap(), 1.959963986120195);
+    assert_eq!(distribution::inverse_normal_cdf(0.1).unwrap(), -1.2815515641401563);
+    assert_eq!(distribution::inverse_normal_cdf(0.5).unwrap(), 0.0);
+    assert!(undefined_result.is_nan());
+}
