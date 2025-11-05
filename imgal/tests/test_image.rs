1aee0a2230d60448ac316dd8bccf280f11f233d1
Edward Evans <elevans2@wisc.edu>
2025-11-05 13:25:03 -0600

    Increase the gradient in the test image

--- imgal/tests/test_image.rs
+++ imgal/tests/test_image.rs
@@ -7,10 +7,10 @@ use imgal::statistics::min_max;
 fn image_histogram() {
     // create data with known values and get the histogram
     let data = Array2::from_shape_fn((20, 20), |(i, j)| {
-        if i < 15 {
+        if i < 5 {
             0
         } else {
-            ((i - 15) * 20 + j) as u16
+            ((i - 5) * 20 + j) as u16
         }
     });
     let hist = image::histogram(data.view().into_dyn(), Some(20));
@@ -19,10 +19,10 @@ fn image_histogram() {
     let arr = Array::from_vec(hist);
     let mm = min_max(arr.view().into_dyn());
 
-    assert_eq!(mm.0, 5);
-    assert_eq!(mm.1, 305);
-    assert_eq!(arr[0], 305);
-    assert_eq!(arr[10], 5);
+    assert_eq!(mm.0, 15);
+    assert_eq!(mm.1, 115);
+    assert_eq!(arr[0], 115);
+    assert_eq!(arr[10], 15);
     assert_eq!(arr.len(), 20);
 }
 
