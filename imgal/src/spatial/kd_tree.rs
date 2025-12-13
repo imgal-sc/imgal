f5e0927f02e0cb926818e66b4e888a71a6aac2a9
Edward Evans <elevans2@wisc.edu>
2025-12-13 11:24:28 -0600

    Improve kd-tree docs, add Node impl and new fn

--- imgal/src/spatial/kd_tree.rs
+++ imgal/src/spatial/kd_tree.rs
@@ -1,27 +1,44 @@
-use ndarray::{ArrayViewD};
+use ndarray::ArrayViewD;
 
 use crate::traits::numeric::AsNumeric;
 
-/// A KD-tree.
+/// An immutable K-d tree for fast spatial queries.
 ///
 /// The KD-tree itself does not *own* its source data but instead uses a view.
 /// This design ensures that imgal's KD-trees are *immutable* once constructed
 /// and are intended for lookups only.
-pub struct KDTree<'a, T>{
+pub struct KDTree<'a, T> {
     pub source: ArrayViewD<'a, T>,
     pub nodes: Vec<Node>,
     pub root: Option<usize>,
 }
 
-/// A KD-tree node.
+/// A K-d-tree node for an immutable K-d tree.
 ///
 /// KD-trees are constructed with `Node`s. These `Nodes` are stored in a
 /// `Vec<Node>` and the `left` and `right` fields store indices into the `Node`
-/// vector.
+/// vector. The axis the split occurs at is stored in `split_ax` and the index
+/// into the source array is stored in the `point_index` field.
 pub struct Node {
-    pub split_dim: usize,
+    pub split_ax: usize,
     pub point_index: usize,
     pub left: Option<usize>,
     pub right: Option<usize>,
 }
 
+impl Node {
+    /// Creates a new K-d tree node.
+    pub fn new(
+        split_ax: usize,
+        point_index: usize,
+        left: Option<usize>,
+        right: Option<usize>,
+    ) -> Self {
+        Self {
+            split_ax,
+            point_index,
+            left,
+            right,
+        }
+    }
+}
