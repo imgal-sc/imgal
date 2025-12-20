17cde7a0cb525812110d7f10391a543332fd47d6
Edward Evans <elevans2@wisc.edu>
2025-12-20 13:23:10 -0600

    Put the stats tests in alphabetical order

--- imgal/tests/test_statistics.rs
+++ imgal/tests/test_statistics.rs
@@ -40,6 +40,47 @@ fn statistics_linear_percentile() {
 }
 
 #[test]
+fn statistics_max_1d() {
+    let data_f64 = vec![1.0, 5.0, 3.0, 9.0, 2.0];
+    let data_str = vec!["1.0", "5.0", "4.0"];
+    let result_f64 = statistics::max(&data_f64);
+    let result_str = statistics::max(&data_str);
+    assert_eq!(result_f64, 9.0);
+    assert_eq!(result_str, "5.0");
+
+    let data_f64_array: Array1<f64> = Array1::from_vec(data_f64);
+    let data_str_array: Array1<&'static str> = Array1::from_vec(data_str);
+    let result_f64_array = statistics::max(&data_f64_array);
+    let result_str_array = statistics::max(&data_str_array);
+    assert_eq!(result_f64_array, 9.0);
+    assert_eq!(result_str_array, "5.0");
+}
+
+#[test]
+fn statistics_min_1d() {
+    let data = vec![1.0, 5.0, 3.0, 9.0, 2.0];
+    let result = statistics::min(&data);
+    assert_eq!(result, 1.0);
+
+    let data_array: Array1<f64> = Array1::from_vec(data);
+    let result_array = statistics::min(&data_array);
+    assert_eq!(result_array, 1.0);
+}
+
+#[test]
+fn statistics_min_max_1d() {
+    let data = vec![1.0, 5.0, 3.0, 9.0, 2.0];
+    let (min, max) = statistics::min_max(&data);
+    assert_eq!(min, 1.0);
+    assert_eq!(max, 9.0);
+
+    let data_array: Array1<f64> = Array1::from_vec(data);
+    let (min_array, max_array) = statistics::min_max(&data_array);
+    assert_eq!(min_array, 1.0);
+    assert_eq!(max_array, 9.0);
+}
+
+#[test]
 fn statistics_sum() {
     // create some test vecs
     let int_data = vec![2, 5, 10, 23];
@@ -99,44 +140,4 @@ fn statistics_weighted_kendall_tau_b_all_ties_returns_zero() {
     let w = [1.0; 4];
     let tau = statistics::weighted_kendall_tau_b(&a, &b, &w).unwrap();
     assert_eq!(tau, 0.0);
-}
-
-fn statistics_max_1d() {
-    let data_f64 = vec![1.0, 5.0, 3.0, 9.0, 2.0];
-    let data_str = vec!["1.0", "5.0", "4.0"];
-    let result_f64 = statistics::max(&data_f64);
-    let result_str = statistics::max(&data_str);
-    assert_eq!(result_f64, 9.0);
-    assert_eq!(result_str, "5.0");
-
-    let data_f64_array: Array1<f64> = Array1::from_vec(data_f64);
-    let data_str_array: Array1<&'static str> = Array1::from_vec(data_str);
-    let result_f64_array = statistics::max(&data_f64_array);
-    let result_str_array = statistics::max(&data_str_array);
-    assert_eq!(result_f64_array, 9.0);
-    assert_eq!(result_str_array, "5.0");
-}
-
-#[test]
-fn statistics_min_1d() {
-    let data = vec![1.0, 5.0, 3.0, 9.0, 2.0];
-    let result = statistics::min(&data);
-    assert_eq!(result, 1.0);
-
-    let data_array: Array1<f64> = Array1::from_vec(data);
-    let result_array = statistics::min(&data_array);
-    assert_eq!(result_array, 1.0);
-}
-
-#[test]
-fn statistics_min_max_1d() {
-    let data = vec![1.0, 5.0, 3.0, 9.0, 2.0];
-    let (min, max) = statistics::min_max(&data);
-    assert_eq!(min, 1.0);
-    assert_eq!(max, 9.0);
-
-    let data_array: Array1<f64> = Array1::from_vec(data);
-    let (min_array, max_array) = statistics::min_max(&data_array);
-    assert_eq!(min_array, 1.0);
-    assert_eq!(max_array, 9.0);
 }
