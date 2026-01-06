866be19a5c2f1f9009501ab6b7b2212980321dcd
Edward Evans <elevans2@wisc.edu>
2026-01-06 13:52:38 -0600

    Add stats min to benchmarks

--- benches/bench_statistics.rs
+++ benches/bench_statistics.rs
@@ -1,4 +1,4 @@
-use imgal::statistics::{max, min_max};
+use imgal::statistics::{max, min, min_max};
 use ndarray::Array3;
 use rand::Rng;
 
@@ -15,6 +15,26 @@ fn bench_max_parallel(bencher: divan::Bencher, size: usize) {
             Array3::from_shape_fn((size, size, 30), |_| rng.random_range(..=100u32))
         })
         .bench_values(|data| max(&data, true));
+}
+
+#[divan::bench(args = [100, 500, 1000])]
+fn bench_min_parallel(bencher: divan::Bencher, size: usize) {
+    bencher
+        .with_inputs(|| {
+            let mut rng = rand::rng();
+            Array3::from_shape_fn((size, size, 30), |_| rng.random_range(..=100u32))
+        })
+        .bench_values(|data| min(&data, true));
+}
+
+#[divan::bench(args = [100, 500, 1000])]
+fn bench_min_sequential(bencher: divan::Bencher, size: usize) {
+    bencher
+        .with_inputs(|| {
+            let mut rng = rand::rng();
+            Array3::from_shape_fn((size, size, 30), |_| rng.random_range(..=100u32))
+        })
+        .bench_values(|data| min(&data, false));
 }
 
 #[divan::bench(args = [100, 500, 1000])]
