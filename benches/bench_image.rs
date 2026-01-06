3b6445e411e527887ae196c38960481e77930537
Edward Evans <elevans2@wisc.edu>
2026-01-06 15:20:06 -0600

    Add image namespace benchmark

---
+++ benches/bench_image.rs
@@ -0,0 +1,32 @@
+use divan::Bencher;
+
+use imgal::image::histogram;
+use imgal::simulation::gradient::linear_gradient_2d;
+
+const OFFSET: usize = 5;
+const SCALE: f64 = 20.0;
+
+fn main() {
+    divan::main();
+}
+
+// Benchmark the image namespace.
+#[divan::bench(args = [64, 128, 256])]
+fn bench_histogram_parallel(bencher: Bencher, size: usize) {
+    bencher.with_inputs(|| {
+        let data = linear_gradient_2d(OFFSET, SCALE, (size, size));
+        data
+    }).bench_values(|d| {
+            let _hist = histogram(&d, Some(256), true);
+        });
+}
+
+#[divan::bench(args = [64, 128, 256])]
+fn bench_histogram_sequential(bencher: Bencher, size: usize) {
+    bencher.with_inputs(|| {
+        let data = linear_gradient_2d(OFFSET, SCALE, (size, size));
+        data
+    }).bench_values(|d| {
+            let _hist = histogram(&d, Some(256), false);
+        });
+}
