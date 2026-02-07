0fcc55febaf93694f2f448b625f89d7c212ee642
Edward Evans <elevans2@wisc.edu>
2026-02-07 11:55:19 -0600

    Improve SACA colocalization benchmark
    This commit changes the data used in the benchmark
    to use two overlappig (but not completely) logistic
    metaballs in two channels (2D and 3D).

--- benches/bench_colocalization.rs
+++ benches/bench_colocalization.rs
@@ -1,79 +1,97 @@
 use divan;
-use ndarray::{Array2, Array3};
-use rand::Rng;
+use ndarray::{ArrayD, Array2, Ix2, Ix3};
 
 use imgal::colocalization::{saca_2d, saca_3d};
+use imgal::simulation::blob::logistic_metaballs;
 use imgal::threshold::global::otsu_value;
 
 fn main() {
     divan::main();
 }
 
+fn sim_coloc_data(size: usize, n_dims: usize) -> (ArrayD<f64>, ArrayD<f64>){
+    // logistic metaball simulation parameters, blob "A" is centered on the
+    // image and blob "B" is offset by 5 pixels
+    let a_pos = (size / 2) as u16;
+    let b_pos = a_pos + 5;
+    let center_a = Array2::from_shape_vec((1, n_dims), vec![a_pos; n_dims]).unwrap();
+    let center_b = Array2::from_shape_vec((1, n_dims), vec![b_pos; n_dims]).unwrap();
+    let shape = vec![size; n_dims];
+    let radii = vec![(size / 4) as u16; 1];
+    let intensities = vec![20_u16; 1];
+    let falloffs = vec![3_u16; 1];
+    let background = 0;
+    let blob_a = logistic_metaballs(
+        &center_a,
+        &radii,
+        &intensities,
+        &falloffs,
+        background,
+        &shape,
+    )
+    .unwrap();
+    let blob_b = logistic_metaballs(
+        &center_b,
+        &radii,
+        &intensities,
+        &falloffs,
+        background,
+        &shape,
+    )
+    .unwrap();
+
+    (blob_a, blob_b)
+}
+
 // Benchmark the colocalization namespace.
 #[divan::bench(args = [64, 128, 256])]
 fn bench_saca_2d_parallel(bencher: divan::Bencher, size: usize) {
+    let (a, b) = sim_coloc_data(size, 2);
+    let a = a.into_dimensionality::<Ix2>().unwrap();
+    let b = b.into_dimensionality::<Ix2>().unwrap();
+    let a_ths = otsu_value(&a, None).unwrap();
+    let b_ths = otsu_value(&b, None).unwrap();
     bencher
-        .with_inputs(|| {
-            let mut rng = rand::rng();
-            let a = Array2::from_shape_fn((size, size), |_| rng.random::<f64>());
-            let b = Array2::from_shape_fn((size, size), |_| rng.random::<f64>());
-            let a_ths = otsu_value(&a, None).unwrap();
-            let b_ths = otsu_value(&b, None).unwrap();
-
-            (a, b, a_ths, b_ths)
-        })
-        .bench_values(|(a, b, ta, tb)| {
-            let _res = saca_2d(&a, &b, ta, tb, true).unwrap();
+        .bench(|| {
+            let _res = saca_2d(&a, &b, a_ths, b_ths, true).unwrap();
         });
 }
 
 #[divan::bench(args = [64, 128, 256])]
 fn bench_saca_2d_sequential(bencher: divan::Bencher, size: usize) {
+    let (a, b) = sim_coloc_data(size, 2);
+    let a = a.into_dimensionality::<Ix2>().unwrap();
+    let b = b.into_dimensionality::<Ix2>().unwrap();
+    let a_ths = otsu_value(&a, None).unwrap();
+    let b_ths = otsu_value(&b, None).unwrap();
     bencher
-        .with_inputs(|| {
-            let mut rng = rand::rng();
-            let a = Array2::from_shape_fn((size, size), |_| rng.random::<f64>());
-            let b = Array2::from_shape_fn((size, size), |_| rng.random::<f64>());
-            let a_ths = otsu_value(&a, None).unwrap();
-            let b_ths = otsu_value(&b, None).unwrap();
-
-            (a, b, a_ths, b_ths)
-        })
-        .bench_values(|(a, b, ta, tb)| {
-            let _res = saca_2d(&a, &b, ta, tb, false).unwrap();
+        .bench(|| {
+            let _res = saca_2d(&a, &b, a_ths, b_ths, false).unwrap();
         });
 }
 
 #[divan::bench(args = [64, 128, 256])]
 fn bench_saca_3d_parallel(bencher: divan::Bencher, size: usize) {
+    let (a, b) = sim_coloc_data(size, 3);
+    let a = a.into_dimensionality::<Ix3>().unwrap();
+    let b = b.into_dimensionality::<Ix3>().unwrap();
+    let a_ths = otsu_value(&a, None).unwrap();
+    let b_ths = otsu_value(&b, None).unwrap();
     bencher
-        .with_inputs(|| {
-            let mut rng = rand::rng();
-            let a = Array3::from_shape_fn((size, size, 15), |_| rng.random::<f64>());
-            let b = Array3::from_shape_fn((size, size, 15), |_| rng.random::<f64>());
-            let a_ths = otsu_value(&a, None).unwrap();
-            let b_ths = otsu_value(&b, None).unwrap();
-
-            (a, b, a_ths, b_ths)
-        })
-        .bench_values(|(a, b, ta, tb)| {
-            let _res = saca_3d(&a, &b, ta, tb, true).unwrap();
+        .bench(|| {
+            let _res = saca_3d(&a, &b, a_ths, b_ths, true).unwrap();
         });
 }
 
 #[divan::bench(args = [64, 128, 256])]
 fn bench_saca_3d_sequential(bencher: divan::Bencher, size: usize) {
+    let (a, b) = sim_coloc_data(size, 3);
+    let a = a.into_dimensionality::<Ix3>().unwrap();
+    let b = b.into_dimensionality::<Ix3>().unwrap();
+    let a_ths = otsu_value(&a, None).unwrap();
+    let b_ths = otsu_value(&b, None).unwrap();
     bencher
-        .with_inputs(|| {
-            let mut rng = rand::rng();
-            let a = Array3::from_shape_fn((size, size, 15), |_| rng.random::<f64>());
-            let b = Array3::from_shape_fn((size, size, 15), |_| rng.random::<f64>());
-            let a_ths = otsu_value(&a, None).unwrap();
-            let b_ths = otsu_value(&b, None).unwrap();
-
-            (a, b, a_ths, b_ths)
-        })
-        .bench_values(|(a, b, ta, tb)| {
-            let _res = saca_3d(&a, &b, ta, tb, false).unwrap();
+        .bench(|| {
+            let _res = saca_3d(&a, &b, a_ths, b_ths, false).unwrap();
         });
 }
