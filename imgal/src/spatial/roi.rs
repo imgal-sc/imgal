1fec0e3e4312c288360b506a44548dd1fbb3bc70
Edward Evans <elevans2@wisc.edu>
2026-02-07 21:45:15 -0600

    Parallelize roi_map function
    I haven't tested this yet, but in theory this is
    function can now create ROI point cloud HashMaps
    parallelly or sequentially. After some reading I
    learned how to interact with HashMaps even more
    efficiently with `entry().or_insert_with().push()`
    syntax, with `push()` coming from the Vec<Vec<usize>>
    type I store as the point cloud.
    
    I still need to write tests and also write the
    corresponding Python bindings.

--- imgal/src/spatial/roi.rs
+++ imgal/src/spatial/roi.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+use rayon::prelude::*;
+
 use ndarray::{Array2, ArrayBase, AsArray, Dimension, ViewRepr};
 
 /// Create a ROI map from an n-dimensional label image.
@@ -14,37 +16,64 @@ use ndarray::{Array2, ArrayBase, AsArray, Dimension, ViewRepr};
 /// # Arguments
 ///
 /// * `data`: An n-dimensional label image of type u16.
+/// * `parallel`: If `true`, parallel computation is used across multiple
+///   threads. If `false`, sequential single-threaded computation is used.
 ///
 /// # Returns
 ///
 /// * `HashMap<u16, Array2<usize>>`: A ROI HashMap where the keys are the ROI
 ///   labels and values are the ROI point clouds.
-pub fn roi_map<'a, A, D>(data: A) -> HashMap<u64, Array2<usize>>
+pub fn roi_map<'a, A, D>(data: A, parallel: bool) -> HashMap<u64, Array2<usize>>
 where
     A: AsArray<'a, u64, D>,
     D: Dimension,
 {
     let data: ArrayBase<ViewRepr<&'a u64>, D> = data.into();
     let mut roi_map: HashMap<u64, Vec<Vec<usize>>> = HashMap::new();
-    data.view()
-        .into_dyn()
-        .indexed_iter()
-        .filter(|&(_, &v)| v != 0)
-        .for_each(|(p, &v)| {
-            roi_map.entry(v).or_insert_with(|| {
-                let mut cloud: Vec<Vec<usize>> = Vec::new();
-                cloud.push(p.as_array_view().to_vec());
-                cloud
+    let vec_to_arr = |k: u64, v: Vec<Vec<usize>>| {
+        let arr = Array2::from_shape_vec((v.len(), v[0].len()), v.into_iter().flatten().collect())
+            .expect("Failed to reshape ROI point cloud into an Array2<usize>.");
+        (k, arr)
+    };
+    if parallel {
+        data.view()
+            .into_dyn()
+            .indexed_iter()
+            .par_bridge()
+            .filter(|&(_, &v)| v != 0)
+            .fold(
+                || HashMap::new(),
+                |mut map: HashMap<u64, Vec<Vec<usize>>>, (p, &v)| {
+                    map.entry(v)
+                        .or_insert_with(Vec::new)
+                        .push(p.as_array_view().to_vec());
+                    map
+                },
+            )
+            .reduce(
+                || HashMap::new(),
+                |mut map_a, map_b| {
+                    map_b.into_iter().for_each(|(k, mut v)| {
+                        map_a.entry(k).or_insert_with(Vec::new).append(&mut v);
+                    });
+                    map_a
+                },
+            );
+        return roi_map
+            .into_par_iter()
+            .map(|(k, v)| vec_to_arr(k, v))
+            .collect();
+    } else {
+        data.view()
+            .into_dyn()
+            .indexed_iter()
+            .filter(|&(_, &v)| v != 0)
+            .for_each(|(p, &v)| {
+                roi_map
+                    .entry(v)
+                    .or_insert_with(Vec::new)
+                    .push(p.as_array_view().to_vec());
             });
-        });
-
-    roi_map
-        .into_iter()
-        .map(|(k, v)| {
-            let arr =
-                Array2::from_shape_vec((v.len(), v[0].len()), v.into_iter().flatten().collect())
-                    .expect("Failed to reshape ROI point cloud into an Array2<usize>.");
-            (k, arr)
-        })
-        .collect()
+        return roi_map.into_iter().map(|(k, v)| vec_to_arr(k, v)).collect();
+    }
 }
