5f89d4ef9b85a90cdf3fd556af0ba2b160eb3a2d
Edward Evans <elevans2@wisc.edu>
2026-03-04 15:31:52 -0600

    Add MismatchedArrayShape error to duplicate_into

--- crates/imgal/src/copy/copy.rs
+++ crates/imgal/src/copy/copy.rs
@@ -50,6 +50,10 @@ where
 /// * `data_b`: The input n-dimensional array to copy data to.
 /// * `parallel`: If `true`, parallel copying of the inpu tdata is used across
 ///   multiple threads. If `false`, sequential single-threaded copying is used.
+///
+/// # Returns
+///
+/// * `Err(ImgalError)`: If `data_a.shape() != data_b.shape()`.
 pub fn duplicate_into<'a, T, A, D>(
     data_a: A,
     mut data_b: ArrayViewMut<T, D>,
@@ -61,12 +65,21 @@ where
     T: 'a + AsNumeric,
 {
     let data_a: ArrayBase<ViewRepr<&'a T>, D> = data_a.into();
-
+    if data_a.shape() != data_b.shape() {
+        return Err(ImgalError::MismatchedArrayShapes {
+            a_arr_name: "data_a",
+            a_shape: data_a.shape().to_vec(),
+            b_arr_name: "data_b",
+            b_shape: data_b.shape().to_vec(),
+        });
+    }
     if parallel {
         Zip::from(data_a).and(data_b).par_for_each(|&a, b| {
             *b = a;
         });
+        Ok(())
     } else {
         data_b.assign(&data_a);
+        Ok(())
     }
 }
