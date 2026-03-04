bd6cd4615c16fe989cbefc91f3a3e4c05b3d8c36
Edward Evans <elevans2@wisc.edu>
2026-03-04 16:14:24 -0600

    Add gs_map to Python bindings

--- crates/imgal_python/src/child_modules/phasor_module.rs
+++ crates/imgal_python/src/child_modules/phasor_module.rs
@@ -9,14 +9,10 @@ pub fn register_phasor_module(parent_module: &Bound<'_, PyModule>) -> PyResult<(
     let calibration_module = PyModule::new(parent_module.py(), "calibration")?;
     let plot_module = PyModule::new(parent_module.py(), "plot")?;
     let time_domain_module = PyModule::new(parent_module.py(), "time_domain")?;
-
-    // add module to python's sys.modules
     py_import_module("phasor");
     py_import_module("phasor.calibration");
     py_import_module("phasor.plot");
     py_import_module("phasor.time_domain");
-
-    // add phasor::calibration submodule functions
     calibration_module.add_function(wrap_pyfunction!(
         phasor_functions::calibration_calibrate_coords,
         &calibration_module
@@ -33,8 +29,6 @@ pub fn register_phasor_module(parent_module: &Bound<'_, PyModule>) -> PyResult<(
         phasor_functions::calibration_modulation_and_phase,
         &calibration_module
     )?)?;
-
-    // add phasor::plot submodule functions
     plot_module.add_function(wrap_pyfunction!(
         phasor_functions::plot_gs_mask,
         &plot_module
@@ -51,10 +45,12 @@ pub fn register_phasor_module(parent_module: &Bound<'_, PyModule>) -> PyResult<(
         phasor_functions::plot_monoexponential_coords,
         &plot_module
     )?)?;
-
-    // add phasor::time_domain submodule functions
     time_domain_module.add_function(wrap_pyfunction!(
         phasor_functions::time_domain_gs_image,
+        &time_domain_module
+    )?)?;
+    time_domain_module.add_function(wrap_pyfunction!(
+        phasor_functions::time_domain_gs_map,
         &time_domain_module
     )?)?;
     time_domain_module.add_function(wrap_pyfunction!(
@@ -65,8 +61,6 @@ pub fn register_phasor_module(parent_module: &Bound<'_, PyModule>) -> PyResult<(
         phasor_functions::time_domain_real_coord,
         &time_domain_module
     )?)?;
-
-    // attach phasor submodule before attaching to the parent module
     phasor_module.add_submodule(&calibration_module)?;
     phasor_module.add_submodule(&plot_module)?;
     phasor_module.add_submodule(&time_domain_module)?;
