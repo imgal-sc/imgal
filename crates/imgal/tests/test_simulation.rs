50997adf8931856637290ce710bb1f85191ae3b4
Edward Evans <elevans2@wisc.edu>
2026-03-26 11:02:23 -0500

    Add tests for PCG RNG

--- crates/imgal/tests/test_simulation.rs
+++ crates/imgal/tests/test_simulation.rs
@@ -1,10 +1,9 @@
 use ndarray::s;
 
 use imgal::integration::midpoint;
-use imgal::simulation::{decay, instrument, noise};
+use imgal::simulation::{decay, instrument, noise, rng};
 use imgal::statistics::sum;
 
-// simulated bioexponential decay parameters
 const SAMPLES: usize = 256;
 const PERIOD: f64 = 12.5;
 const TAUS: [f64; 2] = [1.0, 3.0];
@@ -13,6 +12,7 @@ const TOTAL_COUNTS: f64 = 5000.0;
 const IRF_CENTER: f64 = 3.0;
 const IRF_WIDTH: f64 = 0.5;
 const SHAPE: (usize, usize) = (10, 10);
+const SEED: u64 = 42;
 
 // helper functions
 fn ensure_within_tolerance(a: f64, b: f64, tolerance: f64) -> bool {
@@ -240,4 +240,43 @@ fn noise_poisson_noise_3d_mut() {
 
     assert_ne!(i_a, i_b);
     assert!(i_a.iter().all(|&x| x >= 0.0));
+}
+
+// test the simulation::rng module
+#[test]
+fn rng_seeded_pcg_next_f32() {
+    let mut prng = rng::Pcg::new(SEED);
+    let rand_vals: Vec<f32> = (0..10).map(|_| prng.next_f32()).collect();
+    let expected: [f32; 10] = [
+        0.38137853,
+        0.6115997,
+        0.86119527,
+        0.006375134,
+        0.56348145,
+        0.120713174,
+        0.2436052,
+        0.13107562,
+        0.5262756,
+        0.7570765,
+    ];
+    assert_eq!(rand_vals, expected);
+}
+
+#[test]
+fn rng_seeded_pcg_next_u32() {
+    let mut prng = rng::Pcg::new(SEED);
+    let rand_vals: Vec<u32> = (0..10).map(|_| prng.next_u32()).collect();
+    let expected: [u32; 10] = [
+        1638008362, 2626800800, 3698805554, 27381156, 2420134610, 518459218, 1046276605, 562965718,
+        2260336501, 3251618892,
+    ];
+    assert_eq!(rand_vals, expected);
+}
+
+#[test]
+fn rng_seeded_pcg_next_u32_range() {
+    let mut prng = rng::Pcg::new(SEED);
+    let rand_vals: Result<Vec<u32>, _> = (0..10).map(|_| prng.next_u32_range(20..50)).collect();
+    let expected: [u32; 10] = [42, 40, 34, 26, 40, 48, 45, 48, 21, 32];
+    assert_eq!(rand_vals.unwrap(), expected);
 }
