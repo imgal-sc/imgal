cfb1c5fa3d16b0ce1db1c1de5334840cf0e1f1a2
Edward Evans <elevans2@wisc.edu>
2026-04-05 14:57:59 -0500

    Rename normalize module to normalization

--- crates/imgal/src/image/mod.rs
+++ crates/imgal/src/image/mod.rs
@@ -3,5 +3,5 @@ pub(crate) mod histogram;
 pub use histogram::histogram;
 pub use histogram::histogram_bin_midpoint;
 pub use histogram::histogram_bin_range;
-pub(crate) mod normalize;
-pub use normalize::percentile_normalize;
+pub(crate) mod normalization;
+pub use normalization::percentile_normalize;
