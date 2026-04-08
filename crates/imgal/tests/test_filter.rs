a005f0fb40497b01560ab09ff0948173346a1d7c
Edward Evans <elevans2@wisc.edu>
2026-04-08 16:55:24 -0500

    Refactor the filter namespace tests

--- crates/imgal/tests/test_filter.rs
+++ crates/imgal/tests/test_filter.rs
@@ -1,8 +1,10 @@
-use imgal::filter;
-use imgal::simulation::{decay, instrument};
+use imgal::error::ImgalError;
+use imgal::filter::{fft_convolve_1d, fft_deconvolve_1d};
+use imgal::simulation::decay::{gaussian_exponential_decay_1d, ideal_exponential_decay_1d};
+use imgal::simulation::instrument::gaussian_irf_1d;
 use imgal::statistics::sum;
 
-// simulated bioexponential decay parameters, unit is nanoseconds
+const TOLERANCE: f64 = 1e-10;
 const SAMPLES: usize = 256;
 const PERIOD: f64 = 12.5;
 const TAUS: [f64; 2] = [1.0, 3.0];
@@ -11,47 +13,32 @@ const TOTAL_COUNTS: f64 = 5000.0;
 const IRF_CENTER: f64 = 3.0;
 const IRF_WIDTH: f64 = 0.5;
 
-// helper functions
-fn ensure_within_tolerance(a: f64, b: f64, tolerance: f64) -> bool {
-    (a - b).abs() < tolerance
+fn approx_equal(a: f64, b: f64) -> bool {
+    (a - b).abs() < TOLERANCE
 }
 
+/// Tests that `fft_convolve_1d` returns the expected values for photon count,
+/// and a point on the curve of an ideal bioexponential decay curve convolved
+/// with a Gaussian IRF.
 #[test]
-fn filter_fft_convolve_1d() {
-    // simulate two signals to convolve
-    let a = decay::ideal_exponential_decay_1d(SAMPLES, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS)
-        .unwrap();
-    let b = instrument::gaussian_irf_1d(SAMPLES, PERIOD, IRF_CENTER, IRF_WIDTH);
-    let conv_par = filter::fft_convolve_1d(&a, &b, true);
-    let conv_seq = filter::fft_convolve_1d(&a, &b, false);
-
-    // check curve photon count and a point on the curve (near max)
-    assert!(ensure_within_tolerance(
-        sum(&conv_par, false),
-        4960.5567668085005,
-        1e-12
-    ));
-    assert!(ensure_within_tolerance(
-        conv_par[68],
-        135.7148429095218,
-        1e-12
-    ));
-    assert!(ensure_within_tolerance(
-        sum(&conv_seq, false),
-        4960.5567668085005,
-        1e-12
-    ));
-    assert!(ensure_within_tolerance(
-        conv_seq[68],
-        135.7148429095218,
-        1e-12
-    ));
+fn filter_fft_convolve_1d_expected_results() -> Result<(), ImgalError> {
+    let decay_arr = ideal_exponential_decay_1d(SAMPLES, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS)?;
+    let irf_arr = gaussian_irf_1d(SAMPLES, PERIOD, IRF_CENTER, IRF_WIDTH);
+    let conv_par = fft_convolve_1d(&decay_arr, &irf_arr, true);
+    let conv_seq = fft_convolve_1d(&decay_arr, &irf_arr, false);
+    assert!(approx_equal(sum(&conv_par, false), 4960.5567668085));
+    assert!(approx_equal(sum(&conv_seq, false), 4960.5567668085));
+    assert!(approx_equal(conv_par[68], 135.7148429095));
+    assert!(approx_equal(conv_seq[68], 135.7148429095));
+    Ok(())
 }
 
+/// Tests that `fft_deconvolve_1d` returns the expected values for array sum and
+/// a point on the curve of the deconvolved result (the recovered simulated
+/// IRF).
 #[test]
-fn filter_fft_deconvolve_1d() {
-    // simulate two signals to deconvolve
-    let a = decay::gaussian_exponential_decay_1d(
+fn filter_fft_deconvolve_1d_expected_results() -> Result<(), ImgalError> {
+    let gauss_decay_arr = gaussian_exponential_decay_1d(
         SAMPLES,
         PERIOD,
         &TAUS,
@@ -59,32 +46,13 @@ fn filter_fft_deconvolve_1d() {
         TOTAL_COUNTS,
         IRF_CENTER,
         IRF_WIDTH,
-    )
-    .unwrap();
-    let b = decay::ideal_exponential_decay_1d(SAMPLES, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS)
-        .unwrap();
-    let dconv_par = filter::fft_deconvolve_1d(&a, &b, None, true);
-    let dconv_seq = filter::fft_deconvolve_1d(&a, &b, None, false);
-
-    // check curve photon count and a point on the curve (near max)
-    assert!(ensure_within_tolerance(
-        sum(&dconv_par, false),
-        0.9999755326287557,
-        1e-12
-    ));
-    assert!(ensure_within_tolerance(
-        dconv_par[62],
-        0.0905443740772156,
-        1e-12
-    ));
-    assert!(ensure_within_tolerance(
-        sum(&dconv_seq, false),
-        0.9999755326287557,
-        1e-12
-    ));
-    assert!(ensure_within_tolerance(
-        dconv_seq[62],
-        0.0905443740772156,
-        1e-12
-    ));
+    )?;
+    let decay_arr = ideal_exponential_decay_1d(SAMPLES, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS)?;
+    let dconv_par = fft_deconvolve_1d(&gauss_decay_arr, &decay_arr, None, true);
+    let dconv_seq = fft_deconvolve_1d(&gauss_decay_arr, &decay_arr, None, false);
+    assert!(approx_equal(sum(&dconv_par, false), 0.9999755326));
+    assert!(approx_equal(sum(&dconv_seq, false), 0.9999755326));
+    assert!(approx_equal(dconv_par[62], 0.090544374));
+    assert!(approx_equal(dconv_seq[62], 0.090544374));
+    Ok(())
 }
