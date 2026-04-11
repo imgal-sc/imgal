1d02aa89b901b15605a3325b96cb58ab493461ee
Edward Evans <elevans2@wisc.edu>
2026-04-11 12:38:58 -0500

    Refactor spatial tests

--- crates/imgal/tests/test_spatial.rs
+++ crates/imgal/tests/test_spatial.rs
@@ -1,10 +1,12 @@
 use ndarray::array;
 
+use imgal::error::ImgalError;
 use imgal::spatial::KDTree;
 
+/// Tests that `KDTree` can be constructed and returns the expected values when
+/// searching for coordinates and indices.
 #[test]
-fn spatial_kd_tree() {
-    // a 3D point cloud
+fn spatial_kdtree_expected_results() -> Result<(), ImgalError> {
     let cloud = array![
         [-2.7, 3.9, 5.0],
         [0.1, 0.0, 4.0],
@@ -12,23 +14,16 @@ fn spatial_kd_tree() {
         [-3.2, -1.8, -2.3],
         [-4.9, -3.7, -1.1],
     ];
-
-    // query the origin and find points near it
     let tree = KDTree::build(&cloud);
     let query = [0.0, 0.0, 0.0];
-    let result_inds = tree.search_for_indices(&query, 4.3).unwrap();
-    let result_coords = tree.search_for_coords(&query, 4.3).unwrap();
-
-    // check that the has the expected nodes
+    let result_inds = tree.search_for_indices(&query, 4.3)?;
+    let result_coords = tree.search_for_coords(&query, 4.3)?;
     assert!(tree.root.is_some());
     assert_eq!(tree.nodes.len(), 5);
-
-    // check the number of points and the indices
     assert_eq!(result_inds.len(), 2);
     assert_eq!(result_inds, [2, 1]);
-
-    // check the number of points and the coordinates
     assert_eq!(result_coords.dim().0, 2);
     assert_eq!(result_coords.row(0), cloud.row(2));
     assert_eq!(result_coords.row(1), cloud.row(1));
+    Ok(())
 }
