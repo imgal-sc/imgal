9dc8a331fc064a0cd228fc5fe9bab245c53161b2
Edward Evans <elevans2@wisc.edu>
2026-04-14 10:30:58 -0500

    Improve and refactor the threshold tests

--- crates/imgal/tests/test_threshold.rs
+++ crates/imgal/tests/test_threshold.rs
@@ -1,43 +1,102 @@
-use imgal::simulation::gradient::linear_gradient_2d;
-use imgal::threshold::{global, manual};
+use ndarray::arr2;
 
-const OFFSET: usize = 5;
-const SCALE: f64 = 20.0;
-const SHAPE: (usize, usize) = (20, 20);
+use imgal::error::ImgalError;
+use imgal::simulation::blob::gaussian_metaballs;
+use imgal::threshold::global::{otsu_mask, otsu_value};
+use imgal::threshold::manual::manual_mask;
 
-#[test]
-fn threshold_manual_mask() {
-    // create sample data and apply a manual thresohld
-    let data = linear_gradient_2d(OFFSET, SCALE, SHAPE);
-    let mask_seq = manual::manual_mask(&data, 140.0, false);
-    let mask_par = manual::manual_mask(&data, 140.0, true);
+const TOLERANCE: f64 = 1e-10;
+const CENTER: [[f64; 2]; 1] = [[25.0, 25.0]];
+const RADIUS: [f64; 1] = [20.0];
+const INTENSITY: [f64; 1] = [10.0];
+const FALLOFF: [f64; 1] = [2.0];
+const BACKGROUND: f64 = 0.0;
+const SHAPE: [usize; 2] = [50, 50];
 
-    // check points along the threshold boundray
-    assert_eq!(data[[13, 0]], 160.0);
-    assert_eq!(mask_seq[[11, 0]], false);
-    assert_eq!(mask_seq[[13, 0]], true);
-    assert_eq!(mask_par[[11, 0]], false);
-    assert_eq!(mask_par[[13, 0]], true);
+fn approx_equal(a: f64, b: f64) -> bool {
+    (a - b).abs() < TOLERANCE
 }
 
+/// Test that `manual_mask` returns the expected mask by checking its size and
+/// points inside the mask.
 #[test]
-fn threshold_otsu_mask() {
-    let data = linear_gradient_2d(OFFSET, SCALE, SHAPE);
-    let mask_seq = global::otsu_mask(&data, None, false).unwrap();
-    let mask_par = global::otsu_mask(&data, None, true).unwrap();
-
-    // check points along the threshold boundary
-    assert_eq!(mask_seq[[10, 0]], false);
-    assert_eq!(mask_seq[[11, 0]], true);
-    assert_eq!(mask_par[[10, 0]], false);
-    assert_eq!(mask_par[[11, 0]], true);
+fn threshold_manual_mask_expected_results() -> Result<(), ImgalError> {
+    let data = gaussian_metaballs(
+        &arr2(&CENTER),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &SHAPE,
+    )?;
+    let mask_par = manual_mask(&data, 8.5, true);
+    let mask_seq = manual_mask(&data, 8.5, false);
+    let mask_par_size = mask_par
+        .iter()
+        .filter(|&&v| v != false)
+        .fold(0, |acc, _| acc + 1);
+    let mask_seq_size = mask_seq
+        .iter()
+        .filter(|&&v| v != false)
+        .fold(0, |acc, _| acc + 1);
+    assert_eq!(mask_par[[25, 25]], true);
+    assert_eq!(mask_par[[5, 8]], false);
+    assert_eq!(mask_par[[35, 20]], true);
+    assert_eq!(mask_seq[[25, 25]], true);
+    assert_eq!(mask_seq[[5, 8]], false);
+    assert_eq!(mask_seq[[35, 20]], true);
+    assert_eq!(mask_par_size, 421);
+    assert_eq!(mask_seq_size, 421);
+    Ok(())
 }
 
+/// Tests that `otsu_mask` returns the expected mask by checking its size and
+/// points inside the mask.
 #[test]
-fn threshold_otsu_value() {
-    let data = linear_gradient_2d(OFFSET, SCALE, SHAPE);
+fn threshold_otsu_mask_expected_results() -> Result<(), ImgalError> {
+    let data = gaussian_metaballs(
+        &arr2(&CENTER),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &SHAPE,
+    )?;
+    let mask_par = otsu_mask(&data, None, true)?;
+    let mask_seq = otsu_mask(&data, None, false)?;
+    let mask_par_size = mask_par
+        .iter()
+        .filter(|&&v| v != false)
+        .fold(0, |acc, _| acc + 1);
+    let mask_seq_size = mask_seq
+        .iter()
+        .filter(|&&v| v != false)
+        .fold(0, |acc, _| acc + 1);
+    assert_eq!(mask_par[[25, 25]], true);
+    assert_eq!(mask_par[[5, 8]], false);
+    assert_eq!(mask_par[[43, 20]], true);
+    assert_eq!(mask_seq[[25, 25]], true);
+    assert_eq!(mask_seq[[5, 8]], false);
+    assert_eq!(mask_seq[[43, 20]], true);
+    assert_eq!(mask_par_size, 1101);
+    assert_eq!(mask_seq_size, 1101);
+    Ok(())
+}
 
-    // check if Otsu threshold value matches expected
-    assert_eq!(global::otsu_value(&data, None, false).unwrap(), 118.671875);
-    assert_eq!(global::otsu_value(&data, None, true).unwrap(), 118.671875);
+/// Tests that `otsu_value` returns the expected threshold values.
+#[test]
+fn threshold_otsu_value_expected_results() -> Result<(), ImgalError> {
+    let data = gaussian_metaballs(
+        &arr2(&CENTER),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &SHAPE,
+    )?;
+    let threshold_par = otsu_value(&data, None, true)?;
+    let threshold_seq = otsu_value(&data, None, false)?;
+    assert!(approx_equal(threshold_par, 6.4339888756));
+    assert!(approx_equal(threshold_seq, 6.4339888756));
+    Ok(())
 }
