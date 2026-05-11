044dded681ace16899d66593565aae3bbdcdaef6
Edward Evans <elevans2@wisc.edu>
2026-05-11 09:36:57 -0500

    Add errors to halfspace intersection fn

--- crates/imgal/src/spatial/halfspace.rs
+++ crates/imgal/src/spatial/halfspace.rs
@@ -25,27 +25,51 @@ where
     B: AsArray<'a, T, Ix1>,
     T: 'a + AsNumeric,
 {
-    // TODO some error conditions
     let halfspaces: ArrayBase<ViewRepr<&'a T>, Ix2> = halfspaces.into();
     let in_pnt: ArrayBase<ViewRepr<&'a T>, Ix1> = interior_point.into();
-    let [iz, iy, ix] = array::from_fn(|i| in_pnt[i].to_f64());
+    if halfspaces.is_empty() {
+        return Err(ImgalError::InvalidParameterEmptyArray {
+            param_name: "halfspaces",
+        });
+    }
+    if halfspaces.dim().1 != 4 {
+        return Err(ImgalError::InvalidAxisLengthExpected {
+            arr_name: "halfspaces",
+            axis_idx: 1,
+            expected: 4,
+            got: halfspaces.dim().1,
+        });
+    }
+    if in_pnt.len() != 3 {
+        return Err(ImgalError::InvalidAxisLengthExpected {
+            arr_name: "interior_point",
+            axis_idx: 1,
+            expected: 3,
+            got: in_pnt.len(),
+        });
+    }
     let n_h = halfspaces.dim().0;
+    let [iz, iy, ix] = array::from_fn(|i| in_pnt[i].to_f64());
     // we start by convert each halfspace normal vector (primal space) into dual
     // points (dual space)
     let mut dual_points = Array2::<f64>::zeros((n_h, 3));
-    (0..n_h).for_each(|i| {
+    (0..n_h).try_for_each(|i| {
         let [nz, ny, nx, d] = array::from_fn(|j| halfspaces[[i, j]].to_f64());
         let d_prime = nz * iz + ny * iy + nx * ix + d;
-        // TODO if d_prime.abs() < 1e-10 then produce some failure
+        if d_prime.abs() < 1e-12 {
+            return Err(ImgalError::InvalidGeneric {
+                msg: "The interior point lies on a halfspace boundary.",
+            });
+        }
         dual_points[[i, 0]] = nz / -d_prime;
         dual_points[[i, 1]] = ny / -d_prime;
         dual_points[[i, 2]] = nx / -d_prime;
-    });
+        Ok(())
+    })?;
     // constructing convex hull of dual points finds the intersection vertices
     // in primal space after converting back
     let (dual_verts, dual_faces) = quickhull_3d(&dual_points, false)?;
     let n_df = dual_faces.dim().0;
-    // TODO error out if number of faces is == 0.
     let primal_verts: Vec<T> = (0..n_df).fold(Vec::with_capacity(n_df * 3), |mut acc, i| {
         let [a_idx, b_idx, c_idx] = array::from_fn(|j| dual_faces[[i, j]]);
         let [az, ay, ax] = array::from_fn(|j| dual_verts[[a_idx, j]]);
@@ -67,7 +91,13 @@ where
         acc
     });
     let n_pv = primal_verts.len() / 3;
-    // TODO return an error if too few vertices
+    if n_pv < 4 {
+        return Err(ImgalError::InvalidArrayLengthMinimum {
+            arr_name: "primal_verts",
+            arr_len: n_pv,
+            min_len: 4,
+        });
+    }
     let primal_verts = Array2::from_shape_vec((n_pv, 3), primal_verts).unwrap();
     quickhull_3d(&primal_verts, false)
 }
