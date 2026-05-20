65fc87608d7ea9518f4ea1e842a9b1883330e563
Edward Evans <elevans2@wisc.edu>
2026-05-19 20:39:11 -0500

    Improve macro var names, add simple title

--- crates/imgal/src/parallel/mod.rs
+++ crates/imgal/src/parallel/mod.rs
@@ -1,25 +1,26 @@
-/// Do either sequential or parallel work
+/// Dispatch work to either a sequential iterator or parallel iterator with the
+/// requested thread pool count.
 #[macro_export]
 macro_rules! par {
-    ($threads:expr, sequential: $seq_fn:expr, parallel: $par_fn:expr) => {{
-        let resolve_threads = |req: usize| -> usize {
-            let free_threads = std::thread::available_parallelism()
+    ($threads:expr, seq_exp: $seq:expr, par_exp: $par:expr) => {{
+        let resolve_threads = |req: usize| {
+            let max_threads = std::thread::available_parallelism()
                 .map(|n| n.get())
                 .unwrap_or(1);
             match req {
-                0 => free_threads,
-                n => n.min(free_threads),
+                0 => max_threads,
+                n => n.min(max_threads),
             }
         };
-        let get_pool = |t: usize| {
+        let get_pool = |n: usize| {
             rayon::ThreadPoolBuilder::new()
-                .num_threads(t)
+                .num_threads(n)
                 .build()
                 .unwrap()
         };
         match resolve_threads($threads) {
-            1 => $seq_fn,
-            n => get_pool(n).install(|| $par_fn),
+            1 => $seq,
+            n => get_pool(n).install(|| $par),
         }
     }};
 }
