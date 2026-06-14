79a683ed3834a0e0695fd987fa8c5793a07f0e38
Edward Evans <elevans2@wisc.edu>
2026-06-14 17:55:18 -0500

    Use single function benchmarks
    I think these benchmarks are simplier.

--- benches/bench_copy.rs
+++ benches/bench_copy.rs
@@ -1,10 +1,10 @@
-use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 use ndarray::arr2;
 
 use imgal::copy::{copy_into_flat, duplicate};
 use imgal::simulation::blob::gaussian_metaballs;
 
-const SIZES: [usize; 3] = [256, 512, 1024];
+const SIZE: usize = 1024;
 const RADIUS: [f64; 1] = [20.0];
 const INTENSITY: [f64; 1] = [10.0];
 const FALLOFF: [f64; 1] = [2.0];
@@ -12,99 +12,83 @@ const BACKGROUND: f64 = 0.0;
 const THREADS: Option<usize> = Some(0);
 
 fn bench_copy_into_flat_par(c: &mut Criterion) {
-    let mut group = c.benchmark_group("copy_into_flat (Parallel)");
-    SIZES.iter().for_each(|s| {
-        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
-            let center: [[f64; 2]; 1] = [[s as f64 / 2.0, s as f64 / 2.0]];
-            let shape: [usize; 2] = [s, s];
-            let data = gaussian_metaballs(
-                &arr2(&center),
-                &RADIUS,
-                &INTENSITY,
-                &FALLOFF,
-                BACKGROUND,
-                &shape,
-                None,
-            )
-            .unwrap();
-            b.iter(|| {
-                let _ = copy_into_flat(&data, THREADS);
-            });
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    c.bench_function("copy_into_flat (Parallel)", |b| {
+        b.iter(|| {
+            let _ = copy_into_flat(&data, THREADS);
         });
     });
-    group.finish();
 }
 
 fn bench_copy_into_flat_seq(c: &mut Criterion) {
-    let mut group = c.benchmark_group("copy_into_flat (Sequential)");
-    SIZES.iter().for_each(|s| {
-        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
-            let center: [[f64; 2]; 1] = [[s as f64 / 2.0, s as f64 / 2.0]];
-            let shape: [usize; 2] = [s, s];
-            let data = gaussian_metaballs(
-                &arr2(&center),
-                &RADIUS,
-                &INTENSITY,
-                &FALLOFF,
-                BACKGROUND,
-                &shape,
-                None,
-            )
-            .unwrap();
-            b.iter(|| {
-                let _ = copy_into_flat(&data, None);
-            });
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    c.bench_function("copy_into_flat (Sequential)", |b| {
+        b.iter(|| {
+            let _ = copy_into_flat(&data, Some(1));
         });
     });
-    group.finish();
 }
 
 fn bench_duplicate_par(c: &mut Criterion) {
-    let mut group = c.benchmark_group("duplicate (Parallel)");
-    SIZES.iter().for_each(|s| {
-        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
-            let center: [[f64; 2]; 1] = [[s as f64 / 2.0, s as f64 / 2.0]];
-            let shape: [usize; 2] = [s, s];
-            let data = gaussian_metaballs(
-                &arr2(&center),
-                &RADIUS,
-                &INTENSITY,
-                &FALLOFF,
-                BACKGROUND,
-                &shape,
-                None,
-            )
-            .unwrap();
-            b.iter(|| {
-                let _ = duplicate(&data, THREADS);
-            });
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    c.bench_function("duplicate (Parallel)", |b| {
+        b.iter(|| {
+            let _ = duplicate(&data, THREADS);
         });
     });
-    group.finish();
 }
 
 fn bench_duplicate_seq(c: &mut Criterion) {
-    let mut group = c.benchmark_group("duplicate (Sequential)");
-    SIZES.iter().for_each(|s| {
-        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
-            let center: [[f64; 2]; 1] = [[s as f64 / 2.0, s as f64 / 2.0]];
-            let shape: [usize; 2] = [s, s];
-            let data = gaussian_metaballs(
-                &arr2(&center),
-                &RADIUS,
-                &INTENSITY,
-                &FALLOFF,
-                BACKGROUND,
-                &shape,
-                None,
-            )
-            .unwrap();
-            b.iter(|| {
-                let _ = duplicate(&data, None);
-            });
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    c.bench_function("duplicate (Sequential)", |b| {
+        b.iter(|| {
+            let _ = duplicate(&data, Some(1));
         });
     });
-    group.finish();
 }
 
 criterion_group!(
--- benches/bench_image.rs
+++ benches/bench_image.rs
@@ -1,10 +1,10 @@
-use criterion::{BenchmarkId, Criterion, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 use ndarray::arr2;
 
 use imgal::image::histogram;
 use imgal::simulation::blob::gaussian_metaballs;
 
-const SIZES: [usize; 3] = [256, 512, 1024];
+const SIZE: usize = 1024;
 const RADIUS: [f64; 1] = [20.0];
 const INTENSITY: [f64; 1] = [10.0];
 const FALLOFF: [f64; 1] = [2.0];
@@ -12,48 +12,42 @@ const BACKGROUND: f64 = 0.0;
 const THREADS: Option<usize> = Some(0);
 
 fn bench_histogram_par(c: &mut Criterion) {
-    let mut group = c.benchmark_group("histogram (Parallel)");
-    SIZES.iter().for_each(|s| {
-        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
-            let center: [[f64; 2]; 1] = [[s as f64 / 2.0, s as f64 / 2.0]];
-            let shape: [usize; 2] = [s, s];
-            let data = gaussian_metaballs(
-                &arr2(&center),
-                &RADIUS,
-                &INTENSITY,
-                &FALLOFF,
-                BACKGROUND,
-                &shape,
-                None,
-            )
-            .unwrap();
-            b.iter(|| {
-                let _ = histogram(&data, None, THREADS).unwrap();
-            });
-        });
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    c.bench_function("histogram (Parallel)", |b| {
+        b.iter(|| {
+            let _ = histogram(&data, None, THREADS).unwrap();
+        })
     });
 }
 
 fn bench_histogram_seq(c: &mut Criterion) {
-    let mut group = c.benchmark_group("histogram (Sequential)");
-    SIZES.iter().for_each(|s| {
-        group.bench_with_input(BenchmarkId::from_parameter(s), s, |b, &s| {
-            let center: [[f64; 2]; 1] = [[s as f64 / 2.0, s as f64 / 2.0]];
-            let shape: [usize; 2] = [s, s];
-            let data = gaussian_metaballs(
-                &arr2(&center),
-                &RADIUS,
-                &INTENSITY,
-                &FALLOFF,
-                BACKGROUND,
-                &shape,
-                None,
-            )
-            .unwrap();
-            b.iter(|| {
-                let _ = histogram(&data, None, None).unwrap();
-            });
-        });
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    c.bench_function("histogram (Sequential)", |b| {
+        b.iter(|| {
+            let _ = histogram(&data, None, None).unwrap();
+        })
     });
 }
 
