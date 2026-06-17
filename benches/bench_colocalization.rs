736abc6245c1ca69c9a195f5125c1acfb8447bdf
Edward Evans <elevans2@wisc.edu>
2026-06-17 17:53:12 -0500

    Migrate colocalization benchmarks

--- benches/bench_colocalization.rs
+++ benches/bench_colocalization.rs
@@ -1,17 +1,15 @@
-use divan;
+use criterion::{Criterion, criterion_group, criterion_main};
 use ndarray::{Array2, ArrayD, Ix2, Ix3};
 
 use imgal::colocalization::{saca_2d, saca_3d, saca_significance_mask};
+use imgal::prelude::*;
 use imgal::simulation::blob::logistic_metaballs;
 use imgal::threshold::global::otsu_value;
 
-fn main() {
-    divan::main();
-}
+const SIZE: usize = 32;
+const THREADS: Option<usize> = Some(0);
 
-fn sim_coloc_data(size: usize, n_dims: usize) -> (ArrayD<f64>, ArrayD<f64>) {
-    // logistic metaball simulation parameters, blob "A" is centered on the
-    // image and blob "B" is offset by 5 pixels
+fn sim_coloc_data(size: usize, n_dims: usize) -> Result<(ArrayD<f64>, ArrayD<f64>), ImgalError> {
     let a_pos = (size / 2) as u16;
     let b_pos = a_pos + 5;
     let center_a = Array2::from_shape_vec((1, n_dims), vec![a_pos; n_dims]).unwrap();
@@ -20,7 +18,7 @@ fn sim_coloc_data(size: usize, n_dims: usize) -> (ArrayD<f64>, ArrayD<f64>) {
     if n_dims == 2 {
         shape = vec![size; n_dims];
     } else {
-        shape = vec![size, size, 15];
+        shape = vec![3, size, size];
     }
     let radii = vec![(size / 4) as u16];
     let intensities = vec![20_u16];
@@ -33,8 +31,8 @@ fn sim_coloc_data(size: usize, n_dims: usize) -> (ArrayD<f64>, ArrayD<f64>) {
         &falloffs,
         background,
         &shape,
-    )
-    .unwrap();
+        None,
+    )?;
     let blob_b = logistic_metaballs(
         &center_b,
         &radii,
@@ -42,105 +40,77 @@ fn sim_coloc_data(size: usize, n_dims: usize) -> (ArrayD<f64>, ArrayD<f64>) {
         &falloffs,
         background,
         &shape,
-    )
-    .unwrap();
-
-    (blob_a, blob_b)
+        None,
+    )?;
+    Ok((blob_a, blob_b))
 }
 
-// Benchmark the colocalization namespace.
-#[divan::bench(args = [64, 128, 256], sample_count = 5, sample_size = 5)]
-fn bench_saca_2d_parallel(bencher: divan::Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let (a, b) = sim_coloc_data(size, 2);
-            let a = a.into_dimensionality::<Ix2>().unwrap();
-            let b = b.into_dimensionality::<Ix2>().unwrap();
-            let ta = otsu_value(&a, None, false).unwrap();
-            let tb = otsu_value(&b, None, false).unwrap();
-            (a, b, ta, tb)
-        })
-        .bench_values(|(a, b, ta, tb)| {
-            let _res = saca_2d(&a, &b, ta, tb, true).unwrap();
+fn bench_saca_2d(c: &mut Criterion) {
+    let (ch_a, ch_b) = sim_coloc_data(SIZE, 2).unwrap();
+    let ch_a = ch_a.into_dimensionality::<Ix2>().unwrap();
+    let ch_b = ch_b.into_dimensionality::<Ix2>().unwrap();
+    let ta = otsu_value(&ch_a, None, None).unwrap();
+    let tb = otsu_value(&ch_b, None, None).unwrap();
+    let mut group = c.benchmark_group("saca_2d");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = saca_2d(&ch_a, &ch_b, ta, tb, THREADS).unwrap();
         });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = saca_2d(&ch_a, &ch_b, ta, tb, Some(1)).unwrap();
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [64, 128, 256], sample_count = 5, sample_size = 5)]
-fn bench_saca_3d_parallel(bencher: divan::Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let (a, b) = sim_coloc_data(size, 3);
-            let a = a.into_dimensionality::<Ix3>().unwrap();
-            let b = b.into_dimensionality::<Ix3>().unwrap();
-            let ta = otsu_value(&a, None, false).unwrap();
-            let tb = otsu_value(&b, None, false).unwrap();
-            (a, b, ta, tb)
-        })
-        .bench_values(|(a, b, ta, tb)| {
-            let _res = saca_3d(&a, &b, ta, tb, true).unwrap();
+fn bench_saca_3d(c: &mut Criterion) {
+    let (ch_a, ch_b) = sim_coloc_data(SIZE, 3).unwrap();
+    let ch_a = ch_a.into_dimensionality::<Ix3>().unwrap();
+    let ch_b = ch_b.into_dimensionality::<Ix3>().unwrap();
+    let ta = otsu_value(&ch_a, None, None).unwrap();
+    let tb = otsu_value(&ch_b, None, None).unwrap();
+    let mut group = c.benchmark_group("saca_3d");
+    group.sample_size(10);
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = saca_3d(&ch_a, &ch_b, ta, tb, THREADS).unwrap();
         });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = saca_3d(&ch_a, &ch_b, ta, tb, Some(1)).unwrap();
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [64, 128, 256], sample_count = 5, sample_size = 5)]
-fn bench_saca_significance_mask_parallel(bencher: divan::Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let (a, b) = sim_coloc_data(size, 3);
-            let a = a.into_dimensionality::<Ix3>().unwrap();
-            let b = b.into_dimensionality::<Ix3>().unwrap();
-            let ta = otsu_value(&a, None, false).unwrap();
-            let tb = otsu_value(&b, None, false).unwrap();
-            saca_3d(&a, &b, ta, tb, true).unwrap()
-        })
-        .bench_values(|z| {
-            let _res = saca_significance_mask(&z, None, true);
+fn bench_saca_significance_mask(c: &mut Criterion) {
+    let (ch_a, ch_b) = sim_coloc_data(SIZE, 2).unwrap();
+    let ch_a = ch_a.into_dimensionality::<Ix2>().unwrap();
+    let ch_b = ch_b.into_dimensionality::<Ix2>().unwrap();
+    let ta = otsu_value(&ch_a, None, None).unwrap();
+    let tb = otsu_value(&ch_b, None, None).unwrap();
+    let z = saca_2d(&ch_a, &ch_b, ta, tb, Some(0)).unwrap();
+    let mut group = c.benchmark_group("saca_significance_mask");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = saca_significance_mask(&z, None, THREADS);
         });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = saca_significance_mask(&z, None, Some(1));
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [64, 128, 256], sample_count = 5, sample_size = 5)]
-fn bench_saca_2d_sequential(bencher: divan::Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let (a, b) = sim_coloc_data(size, 2);
-            let a = a.into_dimensionality::<Ix2>().unwrap();
-            let b = b.into_dimensionality::<Ix2>().unwrap();
-            let ta = otsu_value(&a, None, false).unwrap();
-            let tb = otsu_value(&b, None, false).unwrap();
-            (a, b, ta, tb)
-        })
-        .bench_values(|(a, b, ta, tb)| {
-            let _res = saca_2d(&a, &b, ta, tb, false).unwrap();
-        });
-}
-
-#[divan::bench(args = [64, 128, 256], sample_count = 5, sample_size = 5)]
-fn bench_saca_3d_sequential(bencher: divan::Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let (a, b) = sim_coloc_data(size, 3);
-            let a = a.into_dimensionality::<Ix3>().unwrap();
-            let b = b.into_dimensionality::<Ix3>().unwrap();
-            let ta = otsu_value(&a, None, false).unwrap();
-            let tb = otsu_value(&b, None, false).unwrap();
-            (a, b, ta, tb)
-        })
-        .bench_values(|(a, b, ta, tb)| {
-            let _res = saca_3d(&a, &b, ta, tb, false).unwrap();
-        });
-}
-
-#[divan::bench(args = [64, 128, 256], sample_count = 5, sample_size = 5)]
-fn bench_saca_significance_mask_sequential(bencher: divan::Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let (a, b) = sim_coloc_data(size, 3);
-            let a = a.into_dimensionality::<Ix3>().unwrap();
-            let b = b.into_dimensionality::<Ix3>().unwrap();
-            let ta = otsu_value(&a, None, false).unwrap();
-            let tb = otsu_value(&b, None, false).unwrap();
-            saca_3d(&a, &b, ta, tb, true).unwrap()
-        })
-        .bench_values(|z| {
-            let _res = saca_significance_mask(&z, None, false);
-        });
-}
+criterion_group!(
+    benches,
+    bench_saca_2d,
+    bench_saca_3d,
+    bench_saca_significance_mask,
+);
+criterion_main!(benches);
