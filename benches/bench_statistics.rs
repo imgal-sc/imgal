e0c557ade44db7764eabdabd315a4c21026f9131
Edward Evans <elevans2@wisc.edu>
2026-06-17 21:21:20 -0500

    Migrate statistics benchmarks to criterion

--- benches/bench_statistics.rs
+++ benches/bench_statistics.rs
@@ -1,118 +1,157 @@
-use divan::Bencher;
-use imgal::constants::RNG_SEED;
-use imgal::simulation::rng::Pcg;
+use criterion::{Criterion, criterion_group, criterion_main};
+use ndarray::arr2;
+
+use imgal::simulation::blob::gaussian_metaballs;
 use imgal::statistics::{linear_percentile, max, min, min_max, sum};
-use ndarray::Array3;
 
-fn main() {
-    // Run registered benchmarks.
-    divan::main();
+const SIZE: usize = 1024;
+const RADIUS: [f64; 1] = [20.0];
+const INTENSITY: [f64; 1] = [10.0];
+const FALLOFF: [f64; 1] = [2.0];
+const BACKGROUND: f64 = 0.0;
+const THREADS: Option<usize> = Some(0);
+
+fn bench_linear_percentile(c: &mut Criterion) {
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    let mut group = c.benchmark_group("linear_percentile");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = linear_percentile(&data, 0.9, None, None, THREADS).unwrap();
+        });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = linear_percentile(&data, 0.9, None, None, Some(1)).unwrap();
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [100, 500, 1000])]
-fn bench_linear_percentile(bencher: Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let mut prng = Pcg::new(RNG_SEED);
-            Array3::from_shape_fn((size, size, 30), |_| {
-                prng.next_u32_range(0..=100u32).unwrap()
-            })
-        })
-        .bench_values(|data| linear_percentile(&data, 0.90, None, None, false));
+fn bench_max(c: &mut Criterion) {
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    let mut group = c.benchmark_group("max");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = max(&data, THREADS).unwrap();
+        });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = max(&data, Some(1)).unwrap();
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [100, 500, 1000])]
-fn bench_max_parallel(bencher: Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let mut prng = Pcg::new(RNG_SEED);
-            Array3::from_shape_fn((size, size, 30), |_| {
-                prng.next_u32_range(0..=100u32).unwrap()
-            })
-        })
-        .bench_values(|data| max(&data, true));
+fn bench_min(c: &mut Criterion) {
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    let mut group = c.benchmark_group("min");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = min(&data, THREADS).unwrap();
+        });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = min(&data, Some(1)).unwrap();
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [100, 500, 1000])]
-fn bench_min_parallel(bencher: Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let mut prng = Pcg::new(RNG_SEED);
-            Array3::from_shape_fn((size, size, 30), |_| {
-                prng.next_u32_range(0..=100u32).unwrap()
-            })
-        })
-        .bench_values(|data| min(&data, true));
+fn bench_min_max(c: &mut Criterion) {
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    let mut group = c.benchmark_group("min_max");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = min_max(&data, THREADS).unwrap();
+        });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = min_max(&data, Some(1)).unwrap();
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [100, 500, 1000])]
-fn bench_min_max_parallel(bencher: Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let mut prng = Pcg::new(RNG_SEED);
-            Array3::from_shape_fn((size, size, 30), |_| {
-                prng.next_u32_range(0..=100u32).unwrap()
-            })
-        })
-        .bench_values(|data| min_max(&data, true));
+fn bench_sum(c: &mut Criterion) {
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    let mut group = c.benchmark_group("sum");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = sum(&data, THREADS);
+        });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = sum(&data, Some(1));
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [100, 500, 1000])]
-fn bench_sum_parallel(bencher: Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let mut prng = Pcg::new(RNG_SEED);
-            Array3::from_shape_fn((size, size, 30), |_| {
-                prng.next_u32_range(0..=100u32).unwrap()
-            })
-        })
-        .bench_values(|data| sum(&data, true));
-}
-
-#[divan::bench(args = [100, 500, 1000])]
-fn bench_min_sequential(bencher: Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let mut prng = Pcg::new(RNG_SEED);
-            Array3::from_shape_fn((size, size, 30), |_| {
-                prng.next_u32_range(0..=100u32).unwrap()
-            })
-        })
-        .bench_values(|data| min(&data, false));
-}
-
-#[divan::bench(args = [100, 500, 1000])]
-fn bench_max_sequential(bencher: Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let mut prng = Pcg::new(RNG_SEED);
-            Array3::from_shape_fn((size, size, 30), |_| {
-                prng.next_u32_range(0..=100u32).unwrap()
-            })
-        })
-        .bench_values(|data| max(&data, false));
-}
-
-#[divan::bench(args = [100, 500, 1000])]
-fn bench_min_max_sequential(bencher: Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let mut prng = Pcg::new(RNG_SEED);
-            Array3::from_shape_fn((size, size, 30), |_| {
-                prng.next_u32_range(0..=100u32).unwrap()
-            })
-        })
-        .bench_values(|data| min_max(&data, false));
-}
-
-#[divan::bench(args = [100, 500, 1000])]
-fn bench_sum_sequential(bencher: Bencher, size: usize) {
-    bencher
-        .with_inputs(|| {
-            let mut prng = Pcg::new(RNG_SEED);
-            Array3::from_shape_fn((size, size, 30), |_| {
-                prng.next_u32_range(0..=100u32).unwrap()
-            })
-        })
-        .bench_values(|data| sum(&data, false));
-}
+criterion_group!(
+    benches,
+    bench_linear_percentile,
+    bench_max,
+    bench_min,
+    bench_min_max,
+    bench_sum
+);
+criterion_main!(benches);
