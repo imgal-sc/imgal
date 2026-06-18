a5cf0d1fd99abf6100f1b2a1092e26b536901b00
Edward Evans <elevans2@wisc.edu>
2026-06-18 09:19:50 -0500

    Migrate spatial benchmarks to criterion

--- benches/bench_spatial.rs
+++ benches/bench_spatial.rs
@@ -1,16 +1,12 @@
-use divan::Bencher;
+use criterion::{Criterion, criterion_group, criterion_main};
 use ndarray::Array2;
 
 use imgal::constants::RNG_SEED;
 use imgal::simulation::rng::Pcg;
 use imgal::spatial::KDTree;
 
-fn main() {
-    // run registered benchmarks
-    divan::main();
-}
+const N_POINTS: usize = 1_000_000;
 
-/// Generate a u32 random n-dimensional point cloud
 fn point_cloud(n_points: usize, n_dims: usize) -> Array2<u32> {
     let mut prng = Pcg::new(RNG_SEED);
     let mut cloud: Array2<u32> = Array2::zeros((n_points, n_dims));
@@ -21,25 +17,22 @@ fn point_cloud(n_points: usize, n_dims: usize) -> Array2<u32> {
     cloud
 }
 
-#[divan::bench(args= [1000, 100_000, 1_000_000])]
-fn build_kdtree_3d(bencher: Bencher, n_points: usize) {
-    bencher
-        .with_inputs(|| point_cloud(n_points, 3))
-        .bench_values(|c| {
-            divan::black_box(KDTree::build(&c));
+fn bench_kdtree(c: &mut Criterion) {
+    let data = point_cloud(N_POINTS, 3);
+    let mut group = c.benchmark_group("kdtree");
+    group.bench_function("build", |b| {
+        b.iter(|| {
+            let _ = KDTree::build(&data);
         });
+    });
+    let tree = KDTree::build(&data);
+    let query = [32, 83, 10];
+    group.bench_function("search_for_indices", |b| {
+        b.iter(|| {
+            let _ = tree.search_for_indices(&query, 10.0).unwrap();
+        });
+    });
 }
 
-#[divan::bench(args= [1000, 100_000, 1_000_000])]
-fn search_kdtree_3d(bencher: Bencher, n_points: usize) {
-    let cloud = point_cloud(n_points, 3);
-    bencher
-        .with_inputs(|| {
-            let tree = KDTree::build(&cloud);
-            let query = [32, 83, 10];
-            (tree, query)
-        })
-        .bench_values(|(t, q)| {
-            let _indices = t.search_for_indices(&q, 10.0).unwrap();
-        });
-}
+criterion_group!(benches, bench_kdtree);
+criterion_main!(benches);
