8f6b7d3f0390e94950f38f744d2e4c35ba967993
Edward Evans <elevans2@wisc.edu>
2026-06-18 19:33:37 -0500

    Migrate filter benchmarks to criterion

--- benches/bench_filter.rs
+++ benches/bench_filter.rs
@@ -1,93 +1,63 @@
-use divan::Bencher;
+use criterion::{Criterion, criterion_group, criterion_main};
 
 use imgal::filter::{fft_convolve_1d, fft_deconvolve_1d};
-use imgal::simulation::{decay, instrument};
+use imgal::simulation::decay::{gaussian_exponential_decay_1d, ideal_exponential_decay_1d};
+use imgal::simulation::instrument::gaussian_irf_1d;
 
+const SAMPLES: usize = 5_000_000;
 const PERIOD: f64 = 12.5;
 const TAUS: [f64; 2] = [1.0, 3.0];
 const FRACTIONS: [f64; 2] = [0.7, 0.3];
 const TOTAL_COUNTS: f64 = 5000.0;
 const IRF_CENTER: f64 = 3.0;
 const IRF_WIDTH: f64 = 0.5;
+const THREADS: Option<usize> = Some(0);
 
-fn main() {
-    divan::main();
-}
-
-#[divan::bench(args = [5000, 500_000, 5_000_000], sample_count = 5, sample_size = 5)]
-fn bench_fft_convolve_1d_parallel(bencher: Bencher, samples: usize) {
-    bencher
-        .with_inputs(|| {
-            let a =
-                decay::ideal_exponential_decay_1d(samples, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS)
-                    .unwrap();
-            let b = instrument::gaussian_irf_1d(samples, PERIOD, IRF_CENTER, IRF_WIDTH);
-            (a, b)
-        })
-        .bench_values(|(a, b)| {
-            let _ = fft_convolve_1d(&a, &b, true);
+fn bench_fft_convolve_1d(c: &mut Criterion) {
+    let arr_a =
+        ideal_exponential_decay_1d(SAMPLES, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS, None).unwrap();
+    let arr_b = gaussian_irf_1d(SAMPLES, PERIOD, IRF_CENTER, IRF_WIDTH, None);
+    let mut group = c.benchmark_group("fft_convolve_1d");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = fft_convolve_1d(&arr_a, &arr_b, THREADS);
         });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = fft_convolve_1d(&arr_a, &arr_b, Some(1));
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [5000, 500_000, 5_000_000], sample_count = 5, sample_size = 5)]
-fn bench_fft_deconvolve_1d_parallel(bencher: Bencher, samples: usize) {
-    bencher
-        .with_inputs(|| {
-            let a = decay::gaussian_exponential_decay_1d(
-                samples,
-                PERIOD,
-                &TAUS,
-                &FRACTIONS,
-                TOTAL_COUNTS,
-                IRF_CENTER,
-                IRF_WIDTH,
-            )
-            .unwrap();
-            let b =
-                decay::ideal_exponential_decay_1d(samples, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS)
-                    .unwrap();
-            (a, b)
-        })
-        .bench_values(|(a, b)| {
-            let _ = fft_deconvolve_1d(&a, &b, None, true);
+fn bench_fft_deconvolve_1d(c: &mut Criterion) {
+    let arr_a = gaussian_exponential_decay_1d(
+        SAMPLES,
+        PERIOD,
+        &TAUS,
+        &FRACTIONS,
+        TOTAL_COUNTS,
+        IRF_CENTER,
+        IRF_WIDTH,
+        None,
+    )
+    .unwrap();
+    let arr_b =
+        ideal_exponential_decay_1d(SAMPLES, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS, None).unwrap();
+    let mut group = c.benchmark_group("fft_convolve_1d");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = fft_deconvolve_1d(&arr_a, &arr_b, None, THREADS);
         });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = fft_deconvolve_1d(&arr_a, &arr_b, None, Some(1));
+        });
+    });
+    group.finish();
 }
 
-#[divan::bench(args = [5000, 500_000, 5_000_000], sample_count = 5, sample_size = 5)]
-fn bench_fft_convolve_1d_sequential(bencher: Bencher, samples: usize) {
-    bencher
-        .with_inputs(|| {
-            let a =
-                decay::ideal_exponential_decay_1d(samples, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS)
-                    .unwrap();
-            let b = instrument::gaussian_irf_1d(samples, PERIOD, IRF_CENTER, IRF_WIDTH);
-            (a, b)
-        })
-        .bench_values(|(a, b)| {
-            let _ = fft_convolve_1d(&a, &b, false);
-        });
-}
-
-#[divan::bench(args = [5000, 500_000, 5_000_000], sample_count = 5, sample_size = 5)]
-fn bench_fft_deconvolve_1d_sequential(bencher: Bencher, samples: usize) {
-    bencher
-        .with_inputs(|| {
-            let a = decay::gaussian_exponential_decay_1d(
-                samples,
-                PERIOD,
-                &TAUS,
-                &FRACTIONS,
-                TOTAL_COUNTS,
-                IRF_CENTER,
-                IRF_WIDTH,
-            )
-            .unwrap();
-            let b =
-                decay::ideal_exponential_decay_1d(samples, PERIOD, &TAUS, &FRACTIONS, TOTAL_COUNTS)
-                    .unwrap();
-            (a, b)
-        })
-        .bench_values(|(a, b)| {
-            let _ = fft_deconvolve_1d(&a, &b, None, false);
-        });
-}
+criterion_group!(benches, bench_fft_convolve_1d, bench_fft_deconvolve_1d);
+criterion_main!(benches);
