5684d8d61b9568e99ef5fe58d3d528d88399c152
Edward Evans <elevans2@wisc.edu>
2026-06-28 13:29:24 -0500

    Add copy_into benchmark to copy benchmarks

--- benches/bench_copy.rs
+++ benches/bench_copy.rs
@@ -1,7 +1,7 @@
 use criterion::{Criterion, criterion_group, criterion_main};
-use ndarray::arr2;
+use ndarray::{Array3, arr2};
 
-use imgal::copy::{copy_into_flat, duplicate};
+use imgal::copy::{copy_into, copy_into_flat, duplicate};
 use imgal::simulation::blob::gaussian_metaballs;
 
 const SIZE: usize = 1024;
@@ -10,6 +10,34 @@ const INTENSITY: [f64; 1] = [10.0];
 const FALLOFF: [f64; 1] = [2.0];
 const BACKGROUND: f64 = 0.0;
 const THREADS: Option<usize> = Some(0);
+
+fn bench_copy_into(c: &mut Criterion) {
+    let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
+    let shape = [10, SIZE, SIZE];
+    let data = gaussian_metaballs(
+        &arr2(&center),
+        &RADIUS,
+        &INTENSITY,
+        &FALLOFF,
+        BACKGROUND,
+        &shape,
+        None,
+    )
+    .unwrap();
+    let mut dup = Array3::<f64>::zeros((10, SIZE, SIZE)).into_dyn();
+    let mut group = c.benchmark_group("copy_into");
+    group.bench_function("Parallel", |b| {
+        b.iter(|| {
+            let _ = copy_into(&data, dup.view_mut(), THREADS);
+        });
+    });
+    group.bench_function("Sequential", |b| {
+        b.iter(|| {
+            let _ = copy_into(&data, dup.view_mut(), THREADS);
+        });
+    });
+    group.finish();
+}
 
 fn bench_copy_into_flat(c: &mut Criterion) {
     let center = [[5.0, SIZE as f64 / 2.0, SIZE as f64 / 2.0]];
@@ -65,5 +93,10 @@ fn bench_duplicate(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench_copy_into_flat, bench_duplicate,);
+criterion_group!(
+    benches,
+    bench_copy_into,
+    bench_copy_into_flat,
+    bench_duplicate,
+);
 criterion_main!(benches);
