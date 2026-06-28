40a52f7f2e33ba235aaaa0c8319b5eab977b13bb
Edward Evans <elevans2@wisc.edu>
2026-06-28 13:30:24 -0500

    Improve the copy module docstring

--- crates/imgal/src/copy/mod.rs
+++ crates/imgal/src/copy/mod.rs
@@ -1,4 +1,8 @@
-//! Array copy functions.
+//! Copy n-dimensional image data.
+//!
+//! This module provides *n*-dimensional image copying functions where data can
+//! be duplicated, copied into pre-existing containers (*i.e.* arrays) and
+//! copied into 1D flat arrays.
 
 mod duplicate;
 
