847d43f0505fc0275e14a0f9a39d84d9f62d413c
Edward Evans <elevans2@wisc.edu>
2026-07-02 12:07:58 -0500

    Add improved docstring to par! macro

--- crates/imgal/src/macros/mod.rs
+++ crates/imgal/src/macros/mod.rs
@@ -1,6 +1,18 @@
 /// Dispatch work to either a sequential iterator or parallel execution path
 /// with the requested thread count.
 ///
+/// # Description
+///
+/// This macro dispatches work to either a sequential or parallel execution path
+/// with the requested, max or auto-detected max thread counts.
+///
+/// # Arguments
+///
+/// * `$threads`: An `Option<usize>` thread count. `None` or `Some(1)` runs
+///   sequentially. `Some(0)` uses all available threads. `Some(n)` uses `n`
+///   threads, clamped to the number of available logical CPUs.
+///
+///
 /// # Example
 ///
 /// ```ignore
